f79dbf74cbc508eed6fc19b83b30d3db07494ada
yeon01 <53289716+yeon01@users.noreply.github.com>
2020-04-27 17:28:18 +0900

    Add files via upload

---
+++ burgerking_cy/src/user/UserDAO.java
@@ -0,0 +1,62 @@
+package user;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class UserDAO {
+	private Connection conn;
+	private PreparedStatement pstmt;
+	private ResultSet rs;
+	
+	public UserDAO() {
+		try {
+			String dbURL = "jdbc:mysql://localhost:3306/burgerking";
+			String dbID = "root";
+			String dbPassword = "1234";
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection(dbURL, dbID, dbPassword);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public int login(String userID, String userPassword) {
+		String SQL = "SELECT userPassword FROM USER WHERE userId = ?";
+		try {
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setString(1, userID);
+			rs = pstmt.executeQuery();
+			if (rs.next()) {
+				if(rs.getString(1).equals(userPassword)) 
+					return 1; //로그인 성공
+				else
+					return 0; //비밀번호 불일치	
+			
+			}
+			return -1; //아이디가 있음
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return -2; //데이터베이스 오류
+	}
+	
+	public int join(User user) {
+		String SQL = "INSERT INTO USER VALUES (?, ?, ?, ?, ?, ?, ?)";
+		try {
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setString(1, user.getUserID());
+			pstmt.setString(2, user.getUserPassword());
+			pstmt.setString(3, user.getUserPasswordcheck());
+			pstmt.setString(4, user.getUserName());
+			pstmt.setString(5, user.getUserBth());
+			pstmt.setString(6, user.getUserGender());
+			pstmt.setString(7, user.getUserH_ads());
+			return pstmt.executeUpdate();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		return -1; // 데이터베이스 오류
+	}
+}
